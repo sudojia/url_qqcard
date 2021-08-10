3d357f087cc97423893e359fb8170355673523ba
sudojia <i@imzjw.cn>
2021-08-11 00:53:35 +0800

    添加 json 实体

---
+++ src/main/java/cn/imzjw/card/entity/JsonData.java
@@ -0,0 +1,16 @@
+package cn.imzjw.card.entity;
+
+import lombok.Data;
+
+/**
+ * @author https://blog.imzjw.cn
+ * @date 2021/8/10 23:01
+ */
+@Data
+public class JsonData {
+
+    /**
+     * 文章标题
+     */
+    private String title;
+}
